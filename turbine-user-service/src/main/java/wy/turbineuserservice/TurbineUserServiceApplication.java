ececdd589abcd552c991742a80600bb480e16129
aiyi-rain <2542741590@qq.com>
2023-07-10 16:54:42 +0800

    user-service服务注册

--- turbine-user-service/src/main/java/wy/turbineuserservice/TurbineUserServiceApplication.java
+++ turbine-user-service/src/main/java/wy/turbineuserservice/TurbineUserServiceApplication.java
@@ -2,10 +2,12 @@ package wy.turbineuserservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
 @EnableHystrix
+@EnableDiscoveryClient
 public class TurbineUserServiceApplication {
 
     public static void main(String[] args) {
