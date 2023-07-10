3b6c04801272e0848387314265e8233b2bbf3e8e
aiyi-rain <2542741590@qq.com>
2023-07-10 16:54:23 +0800

    user-service服务注册

---
+++ turbine-hystrix-dashboard/src/test/java/wy/turbinehystrixdashboard/TurbineHystrixDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package wy.turbinehystrixdashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TurbineHystrixDashboardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
